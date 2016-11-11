b8df31d54ca746016f1ac37081d134caf034ac6e
May Miller-Ricci <maymillerricci@gmail.com>
2016-11-11 12:53:57 -0800

    install fast-csv package and write t-shirt data to csv file

--- scraper.js
+++ scraper.js
@@ -1,16 +1,20 @@
 var cheerio = require("cheerio");
 var request = require("request");
 var fs = require("fs");
+var csv = require("fast-csv");
 
 // sync or non-sync versions of file methods??
 
-// if (!fs.existsSync("data")) {
-//   fs.mkdirSync("data");
-// }
+if (!fs.existsSync("data")) {
+  fs.mkdirSync("data");
+}
 
-// var todayDate = new Date().toISOString().slice(0,10);
-// var csv_path = "data/" + todayDate + ".csv";
-// fs.writeFileSync(csv_path, "data");
+var todayDate = new Date().toISOString().slice(0,10);
+var csv_path = "data/" + todayDate + ".csv";
+
+var csvStream = csv.format({ headers: true });
+var writableStream = fs.createWriteStream(csv_path);
+csvStream.pipe(writableStream);
 
 var rootPath = "http://www.shirts4mike.com/"
 request(rootPath, function(error, response, html) {
@@ -19,16 +23,22 @@ request(rootPath, function(error, response, html) {
     $(".products li a").each(function(i, element) {
       var shirtPath = $(this).attr("href");
       var shirtUrl = rootPath + shirtPath;
-      console.log(shirtUrl);
+
       request(shirtUrl, function(error, response, html) {
         if (!error && response.statusCode === 200) {
           var $ = cheerio.load(html);
           var title = $("title").text();
           var price = $(".price").text();
           var imagePath = $(".shirt-picture img").attr("src");
-          console.log(title);
-          console.log(price);
-          console.log(rootPath + imagePath);
+          
+          var metadata = { 
+            Title: title, 
+            Price: price, 
+            ImageURL: rootPath + imagePath, 
+            URL: shirtUrl 
+          }
+
+          csvStream.write(metadata);
         }
       });
     });
