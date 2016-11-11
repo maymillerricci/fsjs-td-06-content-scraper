0c53f7edb67f8f1316efe0e4c4e36fc2661b1dfd
May Miller-Ricci <maymillerricci@gmail.com>
2016-11-11 12:17:07 -0800

    use request and cheerio to scrape t-shirt site and locate t-shirt data

--- scraper.js
+++ scraper.js
@@ -4,10 +4,33 @@ var fs = require("fs");
 
 // sync or non-sync versions of file methods??
 
-if (!fs.existsSync("data")) {
-  fs.mkdirSync("data");
-}
+// if (!fs.existsSync("data")) {
+//   fs.mkdirSync("data");
+// }
 
-var todayDate = new Date().toISOString().slice(0,10);
-var csv_path = "data/" + todayDate + ".csv";
-fs.writeFileSync(csv_path, "data");
+// var todayDate = new Date().toISOString().slice(0,10);
+// var csv_path = "data/" + todayDate + ".csv";
+// fs.writeFileSync(csv_path, "data");
+
+var rootPath = "http://www.shirts4mike.com/"
+request(rootPath, function(error, response, html) {
+  if (!error && response.statusCode === 200) {
+    var $ = cheerio.load(html);
+    $(".products li a").each(function(i, element) {
+      var shirtPath = $(this).attr("href");
+      var shirtUrl = rootPath + shirtPath;
+      console.log(shirtUrl);
+      request(shirtUrl, function(error, response, html) {
+        if (!error && response.statusCode === 200) {
+          var $ = cheerio.load(html);
+          var title = $("title").text();
+          var price = $(".price").text();
+          var imagePath = $(".shirt-picture img").attr("src");
+          console.log(title);
+          console.log(price);
+          console.log(rootPath + imagePath);
+        }
+      });
+    });
+  }
+});
