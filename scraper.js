1feef2ae8f6379e23915e7597e86c7f76f0a5236
May Miller-Ricci <maymillerricci@gmail.com>
2016-11-11 10:40:57 -0800

    use fs to create data folder if it doesn't exist and create csv file with date

--- scraper.js
+++ scraper.js
@@ -1,3 +1,13 @@
 var cheerio = require("cheerio");
 var request = require("request");
 var fs = require("fs");
+
+// sync or non-sync versions of file methods??
+
+if (!fs.existsSync("data")) {
+  fs.mkdirSync("data");
+}
+
+var todayDate = new Date().toISOString().slice(0,10);
+var csv_path = "data/" + todayDate + ".csv";
+fs.writeFileSync(csv_path, "data");
